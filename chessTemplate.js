ed14b7bc3c6be65aa7f3a290ee52fab25c3b3b0e
AdrienPringle <30927609+AdrienPringle@users.noreply.github.com>
2017-12-29 18:24:56 +0800

    classes for color
    color is now determined by a css class and not by js

--- chessTemplate.js
+++ chessTemplate.js
@@ -3,16 +3,17 @@ var boardSize = 8;
 var tileSize = 50; //in pixels
 let pieces = new Array();
 
+let wk, wq, wr1, wr2, wn1, wn2, wb1, wb2;// vars for white pieces
+
+let bk, bq, br1, br2, bn1, bn2, bb1, bb2;// vars for black pieces
+
 let holdingPiece = undefined;
 
 var tiles = new Array(boardSize);
 for(let i = 0; i < tiles.length; i++){
   tiles[i] = new Array(boardSize);
   for(let j = 0; j < tiles[i].length; j++){
-    let color = 'beige';
-    if(i%2 == j%2){color = 'peru';}
     tiles[i][j] = {
-      color: color,
       cell: undefined,
       piece: undefined,
     }
@@ -27,9 +28,9 @@ function makeBoard(){
     for(let x = 0; x < boardSize; x++){
       let cell = row.insertCell(x);
       tiles[x][y].cell = cell;
-      cell.style = 'background-color:'+tiles[x][y].color;
+      if(x%2 == y%2){cell.classList.add('white');}else{cell.classList.add('black');}
       cell.id = x+','+y;
-      cell.onclick = function (){tileClicked(x,y)};
+      cell.onclick = function(){tileClicked(x,y)};
     }
   }
 }
@@ -53,15 +54,28 @@ function king(column, row, side){
   img.src = 'images/'+side+'k.png';
   let k = piece(100,img,
     function(newRow, newColumn){ //move check
-        let distX = Math.abs(newRow-row);
-        let distY = Math.abs(newColumn-column);
+      console.log(tiles[0][0].piece)
+        column = newColumn;
+        row = newRow;
+        let available = new Array();
+        for(let i = -1; i <= 1; i++){
+          for(let j = -1; j <= 1; j++){
+            let c = column + i;
+            let r = row + j;
 
-        if(distX <= 1 && distY <= 1){ //Check if move is only 1 block away
-          if(!(distX == 0 && distY == 0)){ //make sure the new position is actually new
-            return true;
+            if (i != 0 || j != 0){
+              try{
+                let piece = tiles[c][r].piece;
+                console.log(piece);
+                if (piece == undefined){
+                  available.push([c,r]);
+                }else{
+                }
+              }catch(e){}
+            }
           }
         }
-        return false;
+      return available;
       }, row, column, side);
   return k;
 }
@@ -129,23 +143,23 @@ function pawn(column, row, side){
 
 function defaultBoardInit(){
   pieces = new Array(); //array that holds all active pieces
-  let wk = king(3, 0, "b"); pieces.push(wk);
-  let wq = queen(4, 0, "b"); pieces.push(wq);
-  let wr1 = rook(0, 0, "b"); pieces.push(wr1);
-  let wr2 = rook(7, 0, "b"); pieces.push(wr2);
-  let wn1 = knight(1, 0, "b"); pieces.push(wn1);
-  let wn2 = knight(6, 0, "b"); pieces.push(wn2);
-  let wb1 = bishop(2, 0, "b"); pieces.push(wb1);
-  let wb2 = bishop(5, 0, "b"); pieces.push(wb2);
+  wk = king(3, 0, "b"); pieces.push(wk);
+  wq = queen(4, 0, "b"); pieces.push(wq);
+  wr1 = rook(0, 0, "b"); pieces.push(wr1);
+  wr2 = rook(7, 0, "b"); pieces.push(wr2);
+  wn1 = knight(1, 0, "b"); pieces.push(wn1);
+  wn2 = knight(6, 0, "b"); pieces.push(wn2);
+  wb1 = bishop(2, 0, "b"); pieces.push(wb1);
+  wb2 = bishop(5, 0, "b"); pieces.push(wb2);
 
-  let bk = king(4, 7, "w"); pieces.push(bk);
-  let bq = queen(3, 7, "w"); pieces.push(bq);
-  let br1 = rook(7, 7, "w"); pieces.push(br1);
-  let br2 = rook(0, 7, "w"); pieces.push(br2);
-  let bn1 = knight(6, 7, "w"); pieces.push(bn1);
-  let bn2 = knight(1, 7, "w"); pieces.push(bn2);
-  let bb1 = bishop(5, 7, "w"); pieces.push(bb1);
-  let bb2 = bishop(2, 7, "w"); pieces.push(bb2);
+  bk = king(4, 7, "w"); pieces.push(bk);
+  bq = queen(3, 7, "w"); pieces.push(bq);
+  br1 = rook(7, 7, "w"); pieces.push(br1);
+  br2 = rook(0, 7, "w"); pieces.push(br2);
+  bn1 = knight(6, 7, "w"); pieces.push(bn1);
+  bn2 = knight(1, 7, "w"); pieces.push(bn2);
+  bb1 = bishop(5, 7, "w"); pieces.push(bb1);
+  bb2 = bishop(2, 7, "w"); pieces.push(bb2);
 
 }
 function replaceImages(){
@@ -165,13 +179,14 @@ function movePiece(piece, newX, newY){
   let oldX = piece.column;
   let oldY = piece.row;
 
-  tiles[newX][newY].piece = piece; //add piece in new location
   tiles[piece.column][piece.row].piece = undefined; //get rid of old piece
+  tiles[newX][newY].piece = piece; //add piece in new location
+
   piece.row = newY;
   piece.column = newX; //set position of piece
-
 }
 function tileClicked(x,y){
+  console.log(wk.checkMove(wk.column, wk.row));
   let tile = tiles[x][y];
   if (holdingPiece == undefined){ //true if a piece hasn't already been selected
     if (tile.piece != undefined){ // make sure theres a piece on the tile
